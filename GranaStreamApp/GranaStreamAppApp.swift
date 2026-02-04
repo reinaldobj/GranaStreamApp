a2daa6f0bbf364600ad2f30b0668615fda57d5be
Reinaldo Bispo Pereira Junior <reinaldo.j@gmail.com>
2026-02-04 15:19:22 -0300

    Initial Commit

---
+++ GranaStreamApp/GranaStreamAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GranaStreamAppApp.swift
+//  GranaStreamApp
+//
+//  Created by Reinaldo Junior on 04/02/26.
+//
+
+import SwiftUI
+
+@main
+struct GranaStreamAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
